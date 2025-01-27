88a6e10878874f349092c8ef2bd0c0f4f30c7e77
iExTno <132175289+iExTno@users.noreply.github.com>
2025-01-26 21:06:07 -0300

    Update Permission.lua

--- Permission.lua
+++ Permission.lua
@@ -7,7 +7,7 @@ local Tabela = {
 		["jguuh"] = 				{Tier = 10},
 		["b112014"] = 				{Tier = 5},
 		["ynxvmp"] = 				{Tier = 1},
-		["ProzinhoBr23213"] = 			{Tier = 1000},		
+		["ProzinhoBr23213"] = 			{Tier = 5},		
 		["joseg1999"] = 			{Tier = 1000},	
 		["Renan2809"] = 			{Tier = 5},
 		["NuelNoCra"] = 			{Tier = 2},
